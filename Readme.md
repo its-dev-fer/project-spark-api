3c801163c4bd644b4fddcc2d48477b8e60064e63
Luis Fernando Hernández Morales <fernando@192.168.1.7>
2024-12-09 09:12:40 -0600

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# API para el proyecto Spark
+
+## Stack de tecnologías
+
+- Servidor: Express + TypeScript
+- DB: Postgres
+- ORM: Sequelize
+- Auth: JWT
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+require("dotenv").config();
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(express.json());
+// cómo importar una ruta:
+// app.use("/auth", authRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("OK"); // Health-check
+});
+
+const startServer = async () => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+      });
+};
+
+startServer();
