882690ca94a90e8e4bda52f1e6ca060872631c5a
Norberto López <134337161+KiritoKazut0@users.noreply.github.com>
2024-12-27 11:23:05 -0600

    Update user endpoint (#8)
    * Install CORS package
    
    * 0.0.1
    
    * 0.0.2
    
    * Implements auth service and controller
    
    * feat: complete token services, auth controllers, env setup, and DB initialization
    
    - Implemented token encryption and management services
    - Created controllers for login and refresh token functionality
    - Configured environment variable loading
    - Added automatic table creation for PostgreSQL
    
    * 0.0.3
    
    * Implementation method to search user by email
    
    * refactor(auth): update authentication route handling with .bind() in /login, /refresh, and /create-user
    
    * 0.0.4
    
    * feat(auth): implement authentication middleware to protect API endpoints
    
    * Update user and auth routes, middleware, and registration logic
    
    * feat: implements update controller for user and add middleware in routes update
    
    * 0.0.5

---
+++ src/DTOS/users/UpdateUser.ts
@@ -0,0 +1,5 @@
+export default interface IUpdateUser {
+    email: string;
+    password?: string;
+    name?: string;
+}
--- src/Routes/User.routes.ts
+++ src/Routes/User.routes.ts
@@ -3,12 +3,15 @@ import RegisterUserController from "../Controllers/Users/Register.Controller";
 import TokenService from "../Services/Token.Service";
 import EncryptService from "../Services/Encrypt.Service";
 import UserService from "../Services/User.Service";
-
+import UpdateUserController from "../Controllers/Users/Update.controller";
+import AuthMiddleware from "../Middleware/Auth.Middleware";
 const router = Router();
 
 const tokenService = new TokenService();
 const encryptService = new EncryptService();
 const userService = new UserService();
+const auhtMiddleware = new AuthMiddleware(tokenService);
+const updateUserController = new UpdateUserController(encryptService);
 const userController = new RegisterUserController(
     tokenService,
     encryptService,
@@ -16,5 +19,10 @@ const userController = new RegisterUserController(
 );
 
 router.post("/register", userController.run.bind(userController));
+router.put(
+    "/update",
+    auhtMiddleware.run.bind(auhtMiddleware),
+    updateUserController.run.bind(updateUserController)
+);
 
 export { router as UserRouter };
